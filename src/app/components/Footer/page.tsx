9d4e88cd630a1341ffa4e3654fa5d7f6bb7ba89b
Shahzain Ali <alishahzain604@gmail.com>
2024-12-11 20:03:18 +0500

    Adjust width

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -31,7 +31,7 @@ function Footer() {
               className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-xs outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+          <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded customsm:my-4">
             Sign-Up
           </button>
         </div>
@@ -43,7 +43,7 @@ function Footer() {
       </div>
 
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-bold text-black tracking-widest text-lg mb-3 ml-6">
+        <h2 className="title-font font-bold text-black tracking-widest text-lg mb-3 ml-6 customsm:ml-0 ">
           CATEGORIES
         </h2>
         <nav className="list-none mb-10">
