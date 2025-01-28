6fee5d1d14e413f268631115452f1f970ef005e1
Shahzain Ali <alishahzain604@gmail.com>
2025-01-28 21:40:15 +0500

    Deployment Change

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,12 @@
 import { createClient } from 'next-sanity';
 
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || !process.env.NEXT_PUBLIC_SANITY_DATASET) {
+  console.warn("Sanity credentials not properly loaded");
+}
+
 export const client = createClient({
   projectId: 'kkab2my1',
   dataset: 'production',
   useCdn: true,
-  token: process.env.SANITY_API_TOKEN,
   apiVersion: '2025-01-16'
 });
