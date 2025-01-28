8b2dd1aa63318af930177126fad3be74c2cc00e8
Shahzain Ali <alishahzain604@gmail.com>
2025-01-28 21:30:01 +0500

    ProjextId

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,8 @@
-// client.ts
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: 'kkab2my1',
+  dataset: 'production',
   useCdn: true,
   token: process.env.SANITY_API_TOKEN,
   apiVersion: '2025-01-16'
